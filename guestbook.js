ce462ba87c6bd312577335ee42a7ab332cbad67f
GhaidaMousa <63565336+GhaidaMousa@users.noreply.github.com>
2023-04-08 21:02:18 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-east.functions.appdomain.cloud/api/v1/web/5763d9e5-4bbb-456d-a7e9-9bf6c730ad15/guestbook/read-guestbook-entries-sequence';
+const apiUrl = 'https://us-east.functions.appdomain.cloud/api/v1/web/5763d9e5-4bbb-456d-a7e9-9bf6c730ad15/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
